9d154677797a6671565b85500b5a27a12487fec0
Christian Wilkerson <g0x9a>
2019-01-26 13:18:52 -0800

    added-pathing

--- src/NPC.cpp
+++ src/NPC.cpp
@@ -24,6 +24,22 @@ void NPC::draw(sf::RenderTarget& target, sf::RenderStates state) const {
     // target.draw(m_sprite);
 }
 
-void NPC::pathing(float xDir, float yDir, float vel) {
+void NPC::pathing(float xDir, float yDir, float deltaTime, int react) {
+    int randX = rand()% -1 +1;
+    int randY = rand()% -1 +1;
+    switch(react){
+        //normal reaction
+        case 1:
+            m_sprite.move(xDir*randX*deltaTime,yDir*randY*deltaTime);
+            m_sprite.play("run");
+            m_sprite.setScale(2.f*randX,2.f*rand.Y);
+
+        //scared reaction
+        case 2:
+            m_sprite.move(xDir*randX*deltaTime*3,yDir*randY*deltaTime*3);
+            m_sprite.play("run");
+            m_sprite.setScale(2.f*randX,2.f*rand.Y);
+
+    }
 
 }
