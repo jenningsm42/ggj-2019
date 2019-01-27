3e5cb209928b3ca620dad196e8201d65ecbea1f8
Christian Wilkerson <christian.wilkerson@gmail.com>
2019-01-27 14:50:16 -0800

    buzzing sounds

--- src/InteractiveDoor.cpp
+++ src/InteractiveDoor.cpp
@@ -1,9 +1,13 @@
 #include "InteractiveDoor.hpp"
 #include "Game.hpp"
 
+
+
 InteractiveDoor::InteractiveDoor(Game& game) {
     auto& cache = game.getAssetCache();
     auto texture = cache.getTexture("door.png");
+    auto sound = cache.getSoundBuffer("door.wav");
+    m_sound.setBuffer(*sound);
     m_sprite.setTexture(*texture);
 
     m_sprite.setGridSize(8, 8);
@@ -12,6 +16,7 @@ InteractiveDoor::InteractiveDoor(Game& game) {
     m_sprite.addAnimation("closed", 0, 0, 1, 1.f);
     m_sprite.addAnimation("opening", 1, 0, 2, .3f);
     m_sprite.addAnimation("open", 2, 0, 1, 1.f);
+
 }
 
 ObjectType InteractiveDoor::getType() const noexcept {
@@ -21,8 +26,11 @@ ObjectType InteractiveDoor::getType() const noexcept {
 void InteractiveDoor::action() {
     m_sprite.play("opening");
     m_sprite.playAfter("open");
+    m_sound.play();
+
 }
 
 void InteractiveDoor::reset() {{
-    m_sprite.play("closed");
+        m_sprite.play("closed");
+        m_sound.play();
 }}
